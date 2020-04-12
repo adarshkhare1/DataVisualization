7f7e6f606ac8dcaff0fff2d0339c1158062dab74
Adarsh Khare <adarshkhare@hotmail.com>
2020-04-12 10:29:34 -0700

    Update activecases_rateofchange_movingaverage.r

--- Corona/activecases_rateofchange_movingaverage.r
+++ Corona/activecases_rateofchange_movingaverage.r
@@ -37,16 +37,19 @@ dates <- seq(c(as.Date("2020/3/1")), by = "day", length.out = ncol(d_sums))
 d_sums <- rbind(date=as.Date(dates), d_sums[1:length(countries),])
 d_sums <- data.frame(t(d_sums))
 
-#Create a dataframe for rate of change
+#Create a dataframe for rate of change by skiping date
 d_trend <- ma(d_sums[-1,-1]/d_sums[-nrow(d_sums),-1],5)[-1:-5,]
 colnames(d_trend) <- colnames(d_sums)[-1]
+d_trend$Value <- replace_na(d_trend$Value, 1)
+d_trend$Value[is.infinite(d_trend$Value)] <- 1 
+#add date in trend dataframe
 d_trend <- data.frame(t(d_trend))
 d_trend <- rbind(date=as.Date(tail(dates,-5)), d_trend[1:length(countries),])
+d_trend <- data.frame(t(d_trend))
 
 #create a line plot
 yMarker <- 10^ceiling(log10(max(sapply(d_sums[-1], max))))/20
-d_sums <- d_sums %>% gather(countries,key="Country",value="Value", -date)
-p1 <- d_sums %>% 
+p1 <- d_sums %>% gather(countries,key="Country",value="Value", -date) %>% 
   ggplot(aes(x=as.Date(date, origin = "2020/1/1"),y=Value,col=Country,group=Country)) + 
   geom_line() +
   geom_point() +
@@ -55,12 +58,7 @@ p1 <- d_sums %>%
   scale_y_continuous(breaks = round(seq(0, max(sapply(d_sums[3], max)), by = yMarker),1)) +
   scale_x_date(date_labels = "%b/%d") 
 
-d_trend <- data.frame(t(d_trend))
-d_trend <- d_trend %>% gather(countries,key="Country",value="Value", -date)
-d_trend$Value <- replace_na(d_trend$Value, 1)
-d_trend$Value[is.infinite(d_trend$Value)] <- 1 
-
-p2 <- subset(d_trend, d_trend$Value <= 3) %>% 
+p2 <- d_trend %>% gather(countries,key="Country",value="Value", -date) %>% 
   ggplot(aes(x=as.Date(date, origin = "2020/1/1"),y=Value,col=Country,group=Country)) + 
   geom_smooth(method="loess", formula = y ~ x, se = FALSE) +
 #  geom_line() +
@@ -70,4 +68,3 @@ p2 <- subset(d_trend, d_trend$Value <= 3) %>%
   scale_x_date(date_labels = "%b/%d") 
 
 grid.arrange(p1, p2, nrow = 2, top = "COVID-19 : Active cases and growth rate (Data Source- Johns Hopkins JHU CCSE)")
-
