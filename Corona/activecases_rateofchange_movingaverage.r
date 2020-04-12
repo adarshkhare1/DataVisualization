01ebeb3cd06c72be520f10e3f514191060d00da9
Adarsh Khare <adarshkhare@hotmail.com>
2020-04-12 10:51:01 -0700

    Update activecases_rateofchange_movingaverage.r

--- Corona/activecases_rateofchange_movingaverage.r
+++ Corona/activecases_rateofchange_movingaverage.r
@@ -40,22 +40,20 @@ d_sums <- data.frame(t(d_sums))
 #Create a dataframe for rate of change by skiping date
 d_trend <- ma(d_sums[-1,-1]/d_sums[-nrow(d_sums),-1],5)[-1:-5,]
 colnames(d_trend) <- colnames(d_sums)[-1]
-d_trend$Value <- replace_na(d_trend$Value, 1)
-d_trend$Value[is.infinite(d_trend$Value)] <- 1 
 #add date in trend dataframe
 d_trend <- data.frame(t(d_trend))
 d_trend <- rbind(date=as.Date(tail(dates,-5)), d_trend[1:length(countries),])
 d_trend <- data.frame(t(d_trend))
 
 #create a line plot
-yMarker <- 10^ceiling(log10(max(sapply(d_sums[-1], max))))/20
+yMarker <- 10^ceiling(log10(max(d_sums[-1])))/20
 p1 <- d_sums %>% gather(countries,key="Country",value="Value", -date) %>% 
   ggplot(aes(x=as.Date(date, origin = "2020/1/1"),y=Value,col=Country,group=Country)) + 
   geom_line() +
   geom_point() +
   labs(y="Active cases", x = "Date") +
   theme_economist() + 
-  scale_y_continuous(breaks = round(seq(0, max(sapply(d_sums[3], max)), by = yMarker),1)) +
+  scale_y_continuous(breaks = round(seq(0, max(d_sums[-1]), by = yMarker),1)) +
   scale_x_date(date_labels = "%b/%d") 
 
 p2 <- d_trend %>% gather(countries,key="Country",value="Value", -date) %>% 
@@ -68,3 +66,4 @@ p2 <- d_trend %>% gather(countries,key="Country",value="Value", -date) %>%
   scale_x_date(date_labels = "%b/%d") 
 
 grid.arrange(p1, p2, nrow = 2, top = "COVID-19 : Active cases and growth rate (Data Source- Johns Hopkins JHU CCSE)")
+
