d41d8c0b4d2486098ee9b9097bcbb306eb357d5e
Adarsh Khare <adarshkhare@hotmail.com>
2020-04-12 10:33:11 -0700

    Update analysis2_movingaverage.r

--- Corona/analysis2_movingaverage.r
+++ Corona/analysis2_movingaverage.r
@@ -30,7 +30,6 @@ d_sums <- rbind(date=as.Date(dates), d_sums[1:length(countries),])
 d_sums <- data.frame(t(d_sums))
 
 #Create a dataframe for rate of change
-d_diff <- data.frame(diff(data.matrix(d_sums[,-1])))
 d_trend <- ma(d_sums[-1,-1]/d_sums[-nrow(d_sums),-1],6)[-1:-5,]
 colnames(d_trend) <- colnames(d_sums)[-1]
 d_trend <- data.frame(t(d_trend))
@@ -38,8 +37,7 @@ d_trend <- rbind(date=as.Date(tail(dates,-4)), d_trend[1:length(countries),])
 
 #create a line plot
 yMarker <- 10^ceiling(log10(max(sapply(d_sums[-1], max))))/20
-d_sums <- d_sums %>% gather(countries,key="Country",value="Value", -date)
-p1 <- d_sums %>% 
+p1 <- d_sums %>% gather(countries,key="Country",value="Value", -date) %>% 
   ggplot(aes(x=as.Date(date, origin = "2020/1/1"),y=Value,col=Country,group=Country)) + 
   geom_line() +
   geom_point() +
@@ -49,11 +47,10 @@ p1 <- d_sums %>%
   scale_x_date(date_labels = "%b/%d") 
 
 d_trend <- data.frame(t(d_trend))
-d_trend <- d_trend %>% gather(countries,key="Country",value="Value", -date)
 d_trend$Value <- replace_na(d_trend$Value, 1)
 d_trend$Value[is.infinite(d_trend$Value)] <- 1 
 
-p2 <- subset(d_trend, d_trend$Value <= 3) %>% 
+p2 <- d_trend %>% gather(countries,key="Country",value="Value", -date) %>% 
   ggplot(aes(x=as.Date(date, origin = "2020/1/1"),y=Value,col=Country,group=Country)) + 
   geom_smooth(method="loess", formula = y ~ x, se = FALSE) +
   labs(y="(5 day moving average) growth multiplier", x = "Date") +
