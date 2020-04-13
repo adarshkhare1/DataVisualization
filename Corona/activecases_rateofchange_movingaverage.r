0833a765dbd13e5983e9537902f7a4437cda8916
Adarsh Khare <adarshkhare@hotmail.com>
2020-04-12 19:49:05 -0700

    Refactoring base ggplot2 common code

--- Corona/activecases_rateofchange_movingaverage.r
+++ Corona/activecases_rateofchange_movingaverage.r
@@ -6,7 +6,7 @@ require(ggthemes)
 require(scales)
 require(tidyverse)
 require(gridExtra)
-require(rstudioapi) # make sure you have it installed
+require(rstudioapi)
 
 rm(list = ls())
 setwd(dirname(getActiveDocumentContext()$path ))
@@ -36,26 +36,16 @@ d_trend <- data.frame(t(d_trend))
 d_trend <- rbind(date=as.Date(tail(dates,-5)), d_trend[1:length(countries),])
 d_trend <- data.frame(t(d_trend))
 
-#create a line plot
+#create a line plot for active cases
 yMarker <- 10^ceiling(log10(max(d_sums[-1])))/20
-p1 <- d_sums %>% gather(countries,key="Country",value="Value", -date) %>% 
-  ggplot(aes(x=as.Date(date, origin = "2020/1/1"),y=Value,col=Country,group=Country)) + 
-  geom_line() +
-  geom_point() +
-  labs(y="Active cases", x = "Date") +
-  theme_economist() + 
+p1 <- get_base_plot(d_sums, "Active cases") +
   scale_y_continuous(breaks = round(seq(0, max(d_sums[-1]), by = yMarker),1)) +
-  scale_x_date(date_labels = "%b/%d") 
+  geom_line() +
+  geom_point() 
 
-p2 <- d_trend %>% gather(countries,key="Country",value="Value", -date) %>% 
-  ggplot(aes(x=as.Date(date, origin = "2020/1/1"),y=Value,col=Country,group=Country)) + 
-  geom_smooth(method="loess", formula = y ~ x, se = FALSE) +
-#  geom_line() +
-#  geom_point() +
-  labs(y="(5 day moving average) growth multiplier", x = "Date") +
-  theme_economist() + 
-  scale_x_date(date_labels = "%b/%d") 
+#create a smoothen line plot for growth rate multiplier moving average
+p2 <- get_base_plot(d_trend, "(5 day moving average) growth multiplier") +
+  geom_smooth(method="loess", formula = y ~ x, se = FALSE)
 
 grid.arrange(p1, p2, nrow = 2, top = "COVID-19 : Active cases and growth rate (Data Source- Johns Hopkins JHU CCSE)")
-
 
