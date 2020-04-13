9f3c4c5479972862fdfa4be6d942faad35a34578
Adarsh Khare <adarshkhare@hotmail.com>
2020-04-12 19:49:54 -0700

    Update covid19_functions.r

--- Corona/covid19_functions.r
+++ Corona/covid19_functions.r
@@ -1,4 +1,5 @@
 
+
 #file containing functions for Covid-19 analysis
 
 #calculate moving average
@@ -14,4 +15,13 @@ build_dataframe <- function(cases_file, countries) {
   df_cases <- aggregate(df_cases[-1], by=list(Country=df_cases$Country.Region), FUN=sum)
   rownames(df_cases) <- df_cases$Country
   df_cases[-1]
+}
+
+get_base_plot <- function(d_series, yLabel) {
+  p <- d_series %>% gather(countries,key="Country",value="Value", -date) %>% 
+    ggplot(aes(x=as.Date(date, origin = "2020/1/1"),y=Value,col=Country,group=Country)) + 
+    labs(y=yLabel, x = "Date") +
+    theme_economist() + 
+    scale_x_date(date_labels = "%b/%d")
+  p
 }
