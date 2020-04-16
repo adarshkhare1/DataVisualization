081a255392b53b774143656c3356b5d61651a063
Adarsh Khare <adarshkhare@hotmail.com>
2020-04-15 20:51:33 -0700

    Update analysis2_movingaverage.r

--- Corona/analysis2_movingaverage.r
+++ Corona/analysis2_movingaverage.r
@@ -19,34 +19,33 @@ source("./covid19_functions.r")
 countries <- c("India", "Singapore", "Malaysia", "Japan", "Thailand")
 
 #download the data file
-df_confirmed <- build_dataframe("time_series_covid19_confirmed_global.csv", countries)
-df_recovered <- build_dataframe("time_series_covid19_recovered_global.csv", countries)
-df_deaths <- build_dataframe("time_series_covid19_deaths_global.csv", countries)
+d_sums <- build_dataframe("time_series_covid19_confirmed_global.csv", countries)
 
-#Calculate active cases and add date row
-d_sums <- df_confirmed - df_recovered - df_deaths
+#Add dates 
 dates <- seq(c(as.Date("2020/3/1")), by = "day", length.out = ncol(d_sums))
 d_sums <- rbind(date=as.Date(dates), d_sums[1:length(countries),])
 d_sums <- data.frame(t(d_sums))
 
-#Create a dataframe for rate of change by skiping date
-d_trend <- ma(d_sums[-1,-1]/d_sums[-nrow(d_sums),-1],5)[-1:-5,]
+#Create a dataframe for rate of change
+d_trend <- ma(d_sums[-1,-1]/d_sums[-nrow(d_sums),-1],6)[-1:-5,]
 colnames(d_trend) <- colnames(d_sums)[-1]
-#add date in trend dataframe
 d_trend <- data.frame(t(d_trend))
-d_trend <- rbind(date=as.Date(tail(dates,-5)), d_trend[1:length(countries),])
-d_trend <- data.frame(t(d_trend))
+d_trend <- rbind(date=as.Date(tail(dates,-4)), d_trend[1:length(countries),])
 
-#create a line plot for active cases
+#create a line plot
 yMarker <- 10^ceiling(log10(max(d_sums[-1])))/20
-p1 <- get_base_plot(d_sums, "Active cases") +
+p1 <- get_base_plot(d_sums, "Confirmed cases") +
   scale_y_continuous(breaks = round(seq(0, max(d_sums[-1]), by = yMarker),1)) +
   geom_line(size=0.5) +
   geom_point(size=0.75) 
 
-#create a smoothen line plot for growth rate multiplier moving average
+d_trend <- data.frame(t(d_trend))
+#d_trend$Value <- replace_na(d_trend$Value, 1)
+#d_trend$Value[is.infinite(d_trend$Value)] <- 1 
+
+#create a smoothen line plot for growth rate multiplier trend
 p2 <- get_base_plot(d_trend, "(5 day moving average) growth multiplier") +
   geom_smooth(method="loess", formula = y ~ x, se = FALSE, size=0.6)
 
-grid.arrange(p1, p2, nrow = 2, top = "COVID-19 : Active cases and growth rate (Data Source- Johns Hopkins JHU CCSE)")
 
+grid.arrange(p1, p2, nrow = 2, top = "COVID-19:Confirmed cases and growth rate (Data Source- Johns Hopkins JHU CCSE)")
