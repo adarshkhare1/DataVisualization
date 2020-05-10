d0148952452bc53940c130e28b6c6b5001bed2d2
Adarsh Khare <adarshkhare@hotmail.com>
2020-05-09 22:36:24 -0700

    Visual plot improvements.

--- Corona/activecases_rateofchange_movingaverage.r
+++ Corona/activecases_rateofchange_movingaverage.r
@@ -14,8 +14,10 @@ setwd(dirname(getActiveDocumentContext()$path ))
 source("./covid19_functions.r")
 
 #select countries to plot, support only two countries
-#countries <- c ("US", "Italy", "Germany", "Spain", "France", "China")
-#countries <- c ("India", "Russia", "Brazil", "Canada", "Iran")
+#countries <- c ("US", "France", "Russia", "United_Kingdom")
+#countries <- c ("Germany", "Spain", "Italy", "Turkey", "Iran" )
+#countries <- c ("Belgium", "Brazil", "Canada", "India","Turkey", "Russia")
+#countries <- c ("India", "Peru", "Canada", "Singapore","Brazil")
 countries <- c("India", "Singapore", "Malaysia", "Japan", "Thailand")
 
 #download the data file
@@ -25,7 +27,7 @@ df_deaths <- build_dataframe("time_series_covid19_deaths_global.csv", countries)
 
 #Calculate active cases and add date row
 d_sums <- df_confirmed - df_recovered - df_deaths
-dates <- seq(c(as.Date("2020/3/1")), by = "day", length.out = ncol(d_sums))
+dates <- seq(c(as.Date("2020/3/15")), by = "day", length.out = ncol(d_sums))
 d_sums <- rbind(date=as.Date(dates), d_sums[1:length(countries),])
 d_sums <- data.frame(t(d_sums))
 
@@ -34,14 +36,15 @@ d_trend <- ma(d_sums[-1,-1]/d_sums[-nrow(d_sums),-1],5)[-1:-5,]
 colnames(d_trend) <- colnames(d_sums)[-1]
 #add date in trend dataframe
 d_trend <- data.frame(t(d_trend))
-d_trend <- rbind(date=as.Date(tail(dates,-5)), d_trend[1:length(countries),])
+d_trend <- rbind(date=as.Date(tail(dates,-6)), d_trend[1:length(countries),])
 d_trend <- data.frame(t(d_trend))
 
 #create a line plot for active cases
 yMarker <- 10^ceiling(log10(max(d_sums[-1])))/20
 p1 <- get_base_plot(d_sums, "Active cases") +
-  scale_y_continuous(breaks = round(seq(0, max(d_sums[-1]), by = yMarker),1)) +
-  geom_line(size=0.5) +
+  scale_y_continuous(breaks = round(seq(0, max(d_sums[-1]), by = yMarker),1), labels = scales::comma) +
+#  scale_y_continuous(breaks = round(seq(0, max(d_sums[-1]), by = yMarker),1), labels = human_gbp) +
+  geom_line(size=0.6) +
   geom_point(size=0.75) 
 
 #create a smoothen line plot for growth rate multiplier moving average
@@ -49,4 +52,5 @@ p2 <- get_base_plot(d_trend, "(5 day moving average) growth multiplier") +
   geom_smooth(method="loess", formula = y ~ x, se = FALSE, size=0.6)
 
 grid.arrange(p1, p2, nrow = 2, top = "COVID-19 : Active cases and growth rate (Data Source- Johns Hopkins JHU CCSE)")
+
 
