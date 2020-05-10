b73ec89c6a4af09148d6105cc92e209b0b8f2730
Adarsh Khare <adarshkhare@hotmail.com>
2020-05-09 22:34:44 -0700

    Update covid19_functions.r

--- Corona/covid19_functions.r
+++ Corona/covid19_functions.r
@@ -1,5 +1,3 @@
-
-
 #file containing functions for Covid-19 analysis
 
 #calculate moving average
@@ -11,8 +9,10 @@ build_dataframe <- function(cases_file, countries) {
   cases_url <- paste(base_url,cases_file, sep = "")
   download.file(cases_url, "corona_cases.csv")
   df_cases <- read.csv("corona_cases.csv")
-  df_cases <- subset(df_cases, select=-c(1,3,4,5:43), Country.Region %in% countries & !grepl(".*, .*", Province.State))
+  df_cases$Country.Region <- gsub(' ', '_', df_cases$Country.Region)
+  df_cases <- subset(df_cases, select=-c(1,3,4,5:57), Country.Region %in% countries & !grepl(".*, .*", Province.State))
   df_cases <- aggregate(df_cases[-1], by=list(Country=df_cases$Country.Region), FUN=sum)
+  df_cases$Country <- gsub(' ', '_', df_cases$Country)
   rownames(df_cases) <- df_cases$Country
   df_cases[-1]
 }
@@ -20,7 +20,7 @@ build_dataframe <- function(cases_file, countries) {
 get_base_plot <- function(d_series, yLabel) {
   p <- d_series %>% gather(countries,key="Country",value="Value", -date) %>% 
     ggplot(aes(x=as.Date(date, origin = "2020/1/1"),y=Value,col=Country,group=Country)) + 
-    labs(y=yLabel, x = "Date") +
+    labs(y=yLabel, x = "") +
     theme_economist() + 
     scale_x_date(date_labels = "%b/%d")
   p
